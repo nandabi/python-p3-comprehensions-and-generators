f6886b49b9a8d7aa7308be23c32ede9bcb7b4024
nandabi <valerie.nandabi@student.moringaschool.com>
2024-03-03 17:36:05 +0300

    commit message

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for num in num_list:
+        if num % 2 == 0:
+            evens.append(num)
+    return evens        
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentences = []
+    for sentence in sentence_list:
+        exclamation_sentences.append(sentence + "!")
+    return exclamation_sentences    
